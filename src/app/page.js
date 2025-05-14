67aa56684275a75f82feb1f48331033e17b07266
ADIDIDA21 <adityajigarpatel21@gmail.com>
2025-05-14 21:30:25 +1000

    HeroForm Redirect v1

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
-import Header from "@/components/header";
+import HeroForm from "@/components/forms/HeroForm";
+
 
 export default function Home() {
   return (
@@ -8,13 +9,7 @@ export default function Home() {
           <h1 className="text-6xl font-bold">Your one link<br/>for everything</h1>
           <h2 className="text-gray-500 text-xl mt-6">Share your links, social profiles, contact info and more on one page</h2>
         </div>
-        <form className="inline-flex items-center shadow-lg shadow-gray-500/20">
-          <span className="bg-white py-4 pl-4">branchlink.to/</span>
-          <input type="text" className="py-4 bg-white" placeholder="username"/>
-          <button type="submit" className="bg-blue-500 text-white py-4 px-6">
-            Join for Free
-          </button>
-        </form>
+        <HeroForm />
       </section>
     </main>
 
